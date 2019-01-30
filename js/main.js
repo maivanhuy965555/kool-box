d414ec31dfb8ba82ca0c332c20be5bbfff7d9fa3
Nam Huy <maivanhuy250896@gmail.com>
2019-01-30 19:49:21 +0700

    Update 30/1/2018

---
+++ js/main.js
@@ -0,0 +1,118 @@
+
+
+//Scroll
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll > 50) {
+        $(".logo-header").addClass("header-active");
+        $(".scroll-top").addClass("active-scroll"); // Scroll Menu - Mobile
+    } 
+    else {
+        $(".logo-header").removeClass("header-active");
+        $(".scroll-top").removeClass("active-scroll");
+    }
+    }); //missing );  
+
+// Menu mobile
+
+(function($) {
+    var $main_nav = $('#main-nav');
+    var $toggle = $('.toggle');
+
+    var defaultData = {
+        maxWidth: false,
+        customToggle: $toggle,
+        // navTitle: 'All Categories',
+        levelTitles: true,
+        pushContent: '#container'
+    };
+
+    // add new items to original nav
+    $main_nav.find('li.add').children('a').on('click', function() {
+        var $this = $(this);
+        var $li = $this.parent();
+        var items = eval('(' + $this.attr('data-add') + ')');
+
+        $li.before('<li class="new"><a>' + items[0] + '</a></li>');
+
+        items.shift();
+
+        if (!items.length) {
+            $li.remove();
+        } else {
+            $this.attr('data-add', JSON.stringify(items));
+        }
+
+        Nav.update(true);
+    });
+
+    // call our plugin
+    var Nav = $main_nav.hcOffcanvasNav(defaultData);
+
+    // demo settings update
+
+    const update = (settings) => {
+        if (Nav.isOpen()) {
+            Nav.on('close.once', function() {
+                Nav.update(settings);
+                Nav.open();
+            });
+
+            Nav.close();
+        } else {
+            Nav.update(settings);
+        }
+    };
+
+    $('.actions').find('a').on('click', function(e) {
+        e.preventDefault();
+
+        var $this = $(this).addClass('active');
+        var $siblings = $this.parent().siblings().children('a').removeClass('active');
+        var settings = eval('(' + $this.data('demo') + ')');
+
+        update(settings);
+    });
+
+    $('.actions').find('input').on('change', function() {
+        var $this = $(this);
+        var settings = eval('(' + $this.data('demo') + ')');
+
+        if ($this.is(':checked')) {
+            update(settings);
+        } else {
+            var removeData = {};
+            $.each(settings, function(index, value) {
+                removeData[index] = false;
+            });
+
+            update(removeData);
+        }
+    });
+})(jQuery);
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:10,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:3,
+            nav:true
+        },
+        600:{
+            items:5,
+            nav:false
+        },
+        1000:{
+            items:10,
+            nav:true,
+            loop:true
+        }
+    }
+});
